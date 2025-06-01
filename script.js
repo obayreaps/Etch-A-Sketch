5d4ca85055f719f2a840de69f5dbfe58908d1795
Kevin Le <kevinle29299@gmail.com>
2025-05-31 19:12:04 -0700

    Fix reset board functionality and added color mode indicator

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //add initial color 
 //it changes when user selects different color
 let color = "black";
+let click = false;
 
 function createBoard(size)
 {
@@ -30,10 +31,11 @@ createBoard(16);
 
 
 //Other functions
-function clearBoard()
+function resetBoard()
 {
+    const board = document.querySelector(".board");
     const squares = board.querySelectorAll("div");
-    squares.forEach((div) => div.remove());
+    squares.forEach((div) => div.style.backgroundColor = "white");
 }
 
 function changeSize(input)
@@ -48,10 +50,14 @@ function changeSize(input)
 //colors whenever mouse hovers over board square
 function colorSquare()
 {
-    if(color == "random")
-        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
-    else
-        this.style.backgroundColor = color;
+    if(click)
+    {
+        if(color === "random")
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+        else
+            this.style.backgroundColor = color; 
+    }
+     
 }
 
 //changes the global variable color
@@ -59,3 +65,18 @@ function changeColor(choice)
 {
     color = choice;
 }
+
+//Functionality for click
+document.querySelector("body").addEventListener("click", (e)=> {
+    if(e.target.tagName != 'BUTTON')
+    {
+        click = !click;
+        let colorMode = document.querySelector("#color-mode");
+        if(click)
+        {
+            colorMode.textContent = "Color-Mode: True";
+        }
+        else
+            colorMode.textContent = "Color-Mode: False";
+    }
+});
